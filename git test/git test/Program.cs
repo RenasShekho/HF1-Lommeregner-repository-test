6572ecbf7f0667fa7ebcadae0c6d6ee34f4b6f71
LavenderPixl <annajacobsen1999@hotmail.com>
2023-02-27 11:17:28 +0100

    Base

--- git test/git test/Program.cs
+++ git test/git test/Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Git Test.....");
+﻿int Operator = 0;
+double Result = 0;
+
+
+
+
+
+
+Console.WriteLine("Welcome to The Calculator");
+Console.WriteLine("Choose your operator:");
+Console.WriteLine("1 - Plus | 2 - Minus | 3 - Multiply | 4 - Divide");
+Operator = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Operator);
+
+switch (Operator)
+{
+    case 1:
+        
+        break;
+    case 2:
+
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+
+
+    default:
+        Console.WriteLine("Fejl");
+        break;
+}
